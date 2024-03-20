b5681e7dc3e39a819116edfd1b6536f1485d685f
y88nji <88706932+y88nji@users.noreply.github.com>
2024-03-20 12:52:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 220 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		long num1 = scanner.nextLong();
+		long num2 = scanner.nextLong();
+		long num3 = scanner.nextLong();
+
+		System.out.println(num1 + num2 + num3);
+		
+		
+	}
+
+}
