08d769b5b790cfb2c0e41725b9d653d05c07f0e8
y88nji <88706932+y88nji@users.noreply.github.com>
2024-04-04 16:12:20 +0900

    [Silver III] Title: 바이러스, Time: 224 ms, Memory: 18108 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,10 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	
 	static int V, E, count;
-	static int[][] adj;
-	static boolean[] visited;
+	static int adj[][];
+	static boolean visited[];
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -13,30 +14,40 @@ public class Main {
 		E = sc.nextInt();
 		
 		adj = new int [V+1][V+1];
-		visited = new boolean[V+1];
+		visited = new boolean [V+1];
 		
-		for(int i = 0; i < E; i++) {
+		for(int r = 1; r <= E; r++) {
 			int A = sc.nextInt();
 			int B = sc.nextInt();
 			
 			adj[A][B] = adj[B][A] = 1;
 		}
 		
-		DFS(1);
-		System.out.println(count);
+		count = 0;
+		BFS(1);
+
 		
+		System.out.println(count);
 	}
 
-	private static void DFS(int start) {
+	private static void BFS(int start) {
+
+		Queue<Integer> queue = new LinkedList<>();
+		
+		queue.add(start);
 		visited[start] = true;
 		
-		for(int i = 1; i <= V; i++) {
-			if(visited[i]==false && adj[start][i]==1) {
-				count++;
-				DFS(i);
+		while(!queue.isEmpty()) {
+			int curr = queue.poll();
+			
+			for(int i = 1; i <= V; i++) {
+				if(adj[curr][i] == 1 && visited[i] == false) {
+					queue.add(i);
+					visited[i] = true;
+					count++;
+				}
 			}
 		}
-		
 		
 	}
 }
