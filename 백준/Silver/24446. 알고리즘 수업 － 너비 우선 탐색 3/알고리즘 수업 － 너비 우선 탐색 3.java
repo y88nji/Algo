501d0965b7d4e33778f6ea4cfa141523b1be4c6c
y88nji <88706932+y88nji@users.noreply.github.com>
2024-04-04 14:57:37 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 3, Time: 2540 ms, Memory: 326168 KB -BaekjoonHub

---
+++ 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	static int V, E, R, depth;
+	static boolean[] visited;
+	static List<Integer>[] adjList;
+	static int[] record;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		V = sc.nextInt(); // 정점의 수
+		E = sc.nextInt(); // 간선의 수
+		R = sc.nextInt(); // 시작 위치
+
+		visited = new boolean[V + 1]; // 방문했는지 확인하는 T/F 배열 
+		adjList = new ArrayList[V + 1]; // 인접리스트
+		record = new int[V + 1]; // 깊이를 체크하는 배열
+		Arrays.fill(record, -1); // 깊이 배열 초기화, -1
+
+		// 인접리스트를 생성해줌
+		for (int i = 1; i <= V; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+		
+		// 인접리스트에 값 대입
+		for (int i = 0; i < E; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			adjList[A].add(B);
+			adjList[B].add(A);
+		}
+
+		// 리스트를 정렬함
+		for (int i = 1; i <= V; i++) { 
+			Collections.sort(adjList[i]);
+		}
+
+		// 출력해서 확인해보기
+//		for(List<Integer> data : adjList) {
+//			System.out.println(data);
+//		}
+		
+		depth = 0; // 깊이 변수
+		BFS(R); // 너비우선탐색 시작
+		
+		// 방문한 순서를 출력하기
+		for(int i = 1; i <= V; i++) {
+			System.out.println(record[i]);
+		}
+	}
+
+	private static void BFS(int r) {
+		// 큐 생성
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(r); // 시작정점 큐에 넣기
+		visited[r] = true; // 시작정점 방문처리
+		record[r] = depth;
+		depth++;
+
+		while (!queue.isEmpty()) {
+			int curr = queue.poll();
+			for (int next : adjList[curr]) {
+				if (visited[next] == false) {
+					queue.add(next);
+					visited[next] = true;
+					// 다음 정점의 깊이는 현재 정점의 깊이 + 1
+					 record[next] = record[curr] + 1;
+				}
+			}
+			
+		}
+		
+	}
+
+}
